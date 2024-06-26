5955ce9b41ca218c3d1faea46497432d960d32c3
Wisam <wirrexx@gmail.com>
2024-06-26 17:06:01 +0200

    Update: Commented out line 81

--- fj/fitness_jerk/views.py
+++ fj/fitness_jerk/views.py
@@ -78,7 +78,7 @@ def signup_view(request):
             password = form.cleaned_data.get("password1")
             email = form.cleaned_data.get("email")
             user = User.objects.create_user(username=username, email=email, password=password)  
-            user_info = Members.objects.create(user=user)               # create userinfo related to new user
+            # user_info = Members.objects.create(user=user)               # create userinfo related to new user
             send_mail(                                                  # send welcome email to user
                 subject=f"Welcome to FitBastard",
                 message="You finally made it. You choose to better yourself. Well, good luck with that!",
